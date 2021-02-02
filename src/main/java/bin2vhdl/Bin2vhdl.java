8f7dd756cef8dd53134359e84941f669da30737d
nick-less <nick-less@gmx.de>
2021-02-02 18:21:53 +0100

    converted to maven

--- src/main/java/bin2vhdl/Bin2vhdl.java
+++ src/main/java/bin2vhdl/Bin2vhdl.java
@@ -6,9 +6,10 @@ package bin2vhdl;
  */
 public class Bin2vhdl {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args)  {
         if (args.length == 0) {
-            throw new Exception("Please specify a file.");
+        	System.err.println("Please specify a file.");
+        	System.exit(1);
         }
         Bin2vhdlConvert converter = new Bin2vhdlConvert(args[0]);
         System.out.println(converter.getVhdl());
